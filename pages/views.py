a052f385af95a47fb1d1734f498cc21f1f26c5c2
Shelton Bradfute <slbradfute1@bufss.wtamu.edu>
2026-09-05 16:45:55 -0500

    Display the list on the home page

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+tasks = [
+    {"title": "Lesson 4", "done": True},
+    {"title": "Homework 1", "done": False},
+    {"title": "Quiz 1", "done": True},
+    {"title": "Quiz 2", "done": False},
+    {"title": "Lesson 5", "done": False},
+]
+
+def home(request):
+    return render(request, "home.html", {"tasks": tasks})
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
